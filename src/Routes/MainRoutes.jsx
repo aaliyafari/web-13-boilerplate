99c0365701226a47dc8ba445a39b907c440860cc
Aaliya Farhin <91542100+aaliyafarhin@users.noreply.github.com>
2022-03-16 17:58:49 +0530

    c2
    reactu6

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,6 +1,14 @@
+import {Route,Routes} from "react-router-dom"
+import {Home} from "../components/Home"
+import {About} from "../components/About"
+import {MoviesDashboard} from "../components/MoviesDashboard"
 
 export const MainRoutes = ()=>{
     return(
-        <></>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        </Routes>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
+import {NavLink} from "react-router-dom"
+import Home from "./Home";
 const links = [
   {
     title: "Home",
-    link: "/",
+    to: "/",
     id: "header-link-home",
   },
-  //   add the other link as well
+  {
+    title: "About",
+    to: "/about",
+    id: "header-link-about",
+  },
+  {
+    title: "Movies",
+    to: "/movies",
+    id: "header-link-movies",
+  },
 ];
 export const Navbar = () => {
-  return {
-    //map through the link ad display it in header
-  };
-};
+return(
+  <>
+  {links.map(({title,to},index)=>{
+    return <NavLink key={index} to={to} style={{fontWeight:"bold",color:"black", padding:"10px", textDecoration:"none"}}>{title}</NavLink>
+  })}
+  </>
+)
+}
